067602837e92c77e25d82558d7f42a213d8bc89d
lnkkerst <lnkkerst04@gmail.com>
2023-04-14 22:34:49 +0800

    feat: scaffold

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import App from './App';
+import routes from '~pages';
 
-createApp(App).mount('#app');
+import '@unocss/reset/tailwind.css';
+import 'vuetify/styles';
+import 'virtual:uno.css';
+
+const app = createApp(App);
+app.use(
+  createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+);
+app.use(createVuetify());
+
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import {
+  presetAttributify,
+  presetIcons,
+  presetTagify,
+  presetUno,
+  presetWebFonts,
+  transformerAttributifyJsx,
+  transformerDirectives
+} from 'unocss';
+import UnoCSS from 'unocss/vite';
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx(),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetIcons(),
+        presetWebFonts(),
+        presetAttributify(),
+        presetTagify({
+          prefix: 'un-'
+        })
+      ],
+      transformers: [transformerAttributifyJsx(), transformerDirectives()]
+    }),
+    Pages({
+      extensions: ['vue', 'tsx']
+    })
+  ],
+  resolve: {
+    alias: {
+      '~/': resolve(__dirname, 'src')
+    }
+  }
 });
